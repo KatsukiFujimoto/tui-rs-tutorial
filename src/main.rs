430466927caccf80bee3421dc641274c5e7b615d
KatsukiFujimoto <kf1996cnbl@gmail.com>
2021-12-05 01:55:42 +0900

    tui-rsでできることを把握するために、WidgetのChartをレンダリングできる様にしてみた

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
 use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Modifier, Style};
+use tui::symbols::Marker::{Braille, Dot};
 use tui::symbols::DOT;
 use tui::text::{Span, Spans};
 use tui::widgets::{
-    Block, BorderType, Borders, Cell, List, ListItem, Paragraph, Row, Table, Tabs, Wrap,
+    Axis, Block, BorderType, Borders, Cell, Chart, Dataset, GraphType, List, ListItem, Paragraph,
+    Row, Table, Tabs, Wrap,
 };
 use tui::Terminal;
 
@@ -13,7 +15,7 @@ fn main() -> Result<(), std::io::Error> {
     let stdout = std::io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let terminal = Terminal::new(backend)?;
-    paragraph_sample(terminal);
+    chart_sample(terminal);
     Ok(())
 }
 
@@ -179,5 +181,56 @@ fn paragraph_sample(
             .wrap(Wrap { trim: true });
         let rendering_area = Rect::new(0, 0, 50, 10);
         f.render_widget(paragraph, rendering_area);
+    });
+}
+
+fn chart_sample(
+    mut terminal: Terminal<TermionBackend<termion::raw::RawTerminal<std::io::Stdout>>>,
+) {
+    let _ = terminal.draw(|f| {
+        let datasets = vec![
+            Dataset::default()
+                .name("data1")
+                .marker(Dot)
+                .graph_type(GraphType::Scatter)
+                .style(Style::default().fg(Color::Cyan))
+                .data(&[(0.0, 5.0), (1.0, 6.0), (1.5, 6.434)]),
+            Dataset::default()
+                .name("data2")
+                .marker(Braille)
+                .graph_type(GraphType::Line)
+                .style(Style::default().fg(Color::Magenta))
+                .data(&[(4.0, 5.0), (5.0, 8.0), (7.66, 13.5)]),
+        ];
+        let chart = Chart::new(datasets)
+            .block(Block::default().title("Chart"))
+            .x_axis(
+                Axis::default()
+                    .title(Span::styled("X Axis", Style::default().fg(Color::Red)))
+                    .style(Style::default().fg(Color::White))
+                    .bounds([0.0, 10.0])
+                    .labels(
+                        ["0.0", "5.0", "10.0"]
+                            .iter()
+                            .cloned()
+                            .map(Span::from)
+                            .collect(),
+                    ),
+            )
+            .y_axis(
+                Axis::default()
+                    .title(Span::styled("Y Axis", Style::default().fg(Color::Red)))
+                    .style(Style::default().fg(Color::White))
+                    .bounds([0.0, 10.0])
+                    .labels(
+                        ["0.0", "5.0", "10.0"]
+                            .iter()
+                            .cloned()
+                            .map(Span::from)
+                            .collect(),
+                    ),
+            );
+        let rendering_area = Rect::new(0, 0, 50, 10);
+        f.render_widget(chart, rendering_area);
     });
 }
